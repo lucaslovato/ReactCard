c512762c679ffee1c39d2d99c19a183451af346a
Lucas Lovato <lucaslovato@brickabode.com>
2020-03-06 17:15:16 -0300

    Updates

---
+++ precache-manifest.80466889e7b37799bd6ac067366354d6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef388c9001a14e9c74f89c777cefceb",
+    "url": "/ReactCard/index.html"
+  },
+  {
+    "revision": "b05e7c054f5f230386d8",
+    "url": "/ReactCard/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "468fc49028c5697aabc8",
+    "url": "/ReactCard/static/js/2.05da0624.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/ReactCard/static/js/2.05da0624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05e7c054f5f230386d8",
+    "url": "/ReactCard/static/js/main.3fc3e80a.chunk.js"
+  },
+  {
+    "revision": "ccbfbc4d7f5c361f57b8",
+    "url": "/ReactCard/static/js/runtime-main.eb160a29.js"
+  },
+  {
+    "revision": "9e15b29f43b28de5a27622f7203b9519",
+    "url": "/ReactCard/static/media/GitLab_logo.9e15b29f.png"
+  },
+  {
+    "revision": "74700669d947c08ff9e51b49b33b835b",
+    "url": "/ReactCard/static/media/ciTheBible.74700669.png"
+  },
+  {
+    "revision": "6eb1ee09009290485f0821da594aefa5",
+    "url": "/ReactCard/static/media/dotnet.6eb1ee09.png"
+  },
+  {
+    "revision": "ddb311f54cb183c3025b1bfa21989484",
+    "url": "/ReactCard/static/media/icon.ico.ddb311f5.png"
+  },
+  {
+    "revision": "1b7e959c50672b2b87b309bd85aeb970",
+    "url": "/ReactCard/static/media/jellyfish.1b7e959c.jpg"
+  },
+  {
+    "revision": "82ed6333b3f7e948fb8438009a4ceb3e",
+    "url": "/ReactCard/static/media/nintendo64.82ed6333.gif"
+  },
+  {
+    "revision": "bc6566cec493891adcca95d273e0ec44",
+    "url": "/ReactCard/static/media/omg.bc6566ce.gif"
+  },
+  {
+    "revision": "ac6e48cc609b09078f4946043e2a92ff",
+    "url": "/ReactCard/static/media/superMario.ac6e48cc.gif"
+  }
+]);
